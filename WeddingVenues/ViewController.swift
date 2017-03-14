c8ab8656344ed157a4af115cbf73065db86c2451
Lauren N. Roth <laurennicoleroth@gmail.com>
2017-03-13 22:32:13 -0400

    Initial Commit

---
+++ WeddingVenues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeddingVenues
+//
+//  Created by Lauren Nicole Roth on 3/13/17.
+//  Copyright © 2017 Lauren Nicole Roth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
